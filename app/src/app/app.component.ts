25b94bab080f7209ff5e0ba0484c30659da92eb9
Cole Flannery <jaidencoleflannery@gmail.com>
2025-05-21 15:14:05 -0500

    Basic frontend structure.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component'
+import { MainComponent } from './main/main.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'tinytrail';
 }
