7636c94c1d9dc2a8e87f9afd07becbd4bb1746dd
Cole Flannery <jaidencoleflannery@gmail.com>
2025-05-23 23:59:00 -0500

    UI overhaul / lightmode.

--- app/src/app/main/main.component.ts
+++ app/src/app/main/main.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
-
+export class MainComponent{
+  link = new FormControl();
 }
