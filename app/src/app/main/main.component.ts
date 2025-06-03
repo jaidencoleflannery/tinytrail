d5d80cc8d0cd3056589f5407dda3996cc74f7102
Cole Flannery <jaidencoleflannery@gmail.com>
2025-06-02 21:45:33 -0500

    Post functionality implemented.

--- app/src/app/main/main.component.ts
+++ app/src/app/main/main.component.ts
@@ -1,14 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
-import { NavigationComponent } from "./navigation/navigation.component";
+import { ReactiveFormsModule, FormsModule, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ReactiveFormsModule, NavigationComponent],
+  imports: [ReactiveFormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent{
+  value: any;
+  form: any;
+
   link = new FormControl();
+
+  constructor (private fb: FormBuilder) {
+    this.form = this.fb.group({
+      value: ''
+    })
+  }
+
+    async submit () {
+    if (this.form.get('value').value == null || this.form.get('value').value == '' || this.form.get('value').value == ' ') {
+      return 0;
+    } else {
+      const url = `http://localhost:5137/api/Link/TransformLink?url=` + this.form.get('value').value;
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          credentials: 'include',
+          headers: { 
+            "content-type": "application/json" 
+          },
+        });
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        console.log("DATA:", data);
+      } catch (error) {
+        console.log(error);
+      }
+      this.form.reset(
+        this.value = ''
+      );
+      return null;
+    }
+  }
+
+  dynamicInput(textarea: any): void {
+      textarea.style.height = "auto";
+      textarea.style.height = textarea.scrollHeight + "px";
+  }
+
+  handleEnter(event: KeyboardEvent, textarea: any): void {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      this.submit();
+      this.dynamicInput(textarea);
+    }
+  }
 }
